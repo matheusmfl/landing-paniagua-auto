973b2f8f523cbfbc61f533320cd8163a8987681a
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-05-09 13:45:06 -0300

    Ending Section 3 desktop Version

--- src/components/Section3.tsx
+++ src/components/Section3.tsx
@@ -4,25 +4,30 @@ import hero from '../../public/sofaImg.jpg'
 export function Section3() {
   return (
     <>
-      <section className="py-20 px-4 flex flex-col gap-5 bg-slate-200">
-        <h2 className="text-2xl font-normal text-slate-800">
-          <span className="font-extrabold">Contrate agora</span> seu seguro auto
-          agora mesmo, sem sair de casa e{' '}
-          <span className="font-bold text-yellow-500">sem complicação</span>
-        </h2>
+      <section className=" flex bg-slate-200">
+        <div className="hidden lg:block">
+          <Image src={hero} alt="Imagem ilustrativa" />
+        </div>
+        <div className="flex flex-col gap-5 py-20 px-4 lg:pt-56 lg:pr-20 lg:pl-24">
+          <h2 className="text-2xl lg:text-4xl font-normal text-slate-800">
+            <span className="font-extrabold">Contrate agora</span> seu seguro
+            auto agora mesmo, sem sair de casa e{' '}
+            <span className="font-bold text-yellow-500">sem complicação</span>
+          </h2>
 
-        <span className="font-normal text-base text-slate-800 pb-5">
-          Dê adeus a burocracia e demora para contratar o seu seguro, contamos
-          com corretores credenciados que estão prontos para agilizar e atender
-          qualquer dúvidas.
-        </span>
+          <span className="font-normal text-base lg:text-lg text-slate-800 pb-5">
+            Dê adeus a burocracia e demora para contratar o seu seguro, contamos
+            com corretores credenciados que estão prontos para agilizar e
+            atender qualquer dúvidas.
+          </span>
 
-        <button className="w-full text-bold text-lg text-white rounded-md bg-[#00263F] py-3 shadow-lg">
-          <a href="">Solicitar cotação</a>
-        </button>
+          <button className="w-full text-bold text-lg text-white rounded-md bg-[#00263F] lg:max-w-[284px] py-3 shadow-lg">
+            <a href="">Solicitar cotação</a>
+          </button>
+        </div>
       </section>
 
-      <section className="relative">
+      <section className="relative lg:hidden">
         <div className="absolute top-0 z-[-1]">
           <Image src={hero} alt="Imagem de homem no sofá contratando seguro" />
         </div>
