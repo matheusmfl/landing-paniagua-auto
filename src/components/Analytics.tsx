6fd06dba4d07434b43fbed1f4f943e9b96e7f25d
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-05-11 19:24:43 -0300

    Add tag Script NextJS for analytics

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -1,11 +1,14 @@
+import Script from 'next/script'
+
 export function Analytics() {
   return (
     <>
-      <script
+      <Script
         async
         src={`https://www.googletagmanager.com/gtag/js?id=G-R47RYY2V61`}
       />
-      <script
+      <Script
+        id="Google-analytics"
         dangerouslySetInnerHTML={{
           __html: `
          window.dataLayer = window.dataLayer || [];
