f503713b6fc0bc38fdbf24fe2ff985045975d702
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-05-11 17:52:56 -0300

    Fix bug on text

--- src/components/Section2.tsx
+++ src/components/Section2.tsx
@@ -7,10 +7,8 @@ export function Section2() {
     <section className="py-20 px-5 flex flex-col gap-24 bg-slate-100 items-center lg:max-h-80">
       <div>
         <h2 className="text-cyan-900 font-bold text-xl text-center lg:text-4xl">
-          Confira algumas
-          <span className="text-yellow-500 font-extrabold">
-            seguradoras
-          </span>{' '}
+          Confira algumas{' '}
+          <span className="text-yellow-500 font-extrabold">seguradoras</span>{' '}
           que trabalhamos
         </h2>
       </div>
