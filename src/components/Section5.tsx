952a1a5350e0699d3fad8e210ebc707432786fa3
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-05-11 20:19:13 -0300

    Fix Copy

--- src/components/Section5.tsx
+++ src/components/Section5.tsx
@@ -9,14 +9,14 @@ export function Section5() {
       </div>
       <div className="py-20 px-5 flex flex-col gap-5 lg:w-[90%] lg:py-14 lg:px-20">
         <h2 className="text-2xl text-slate-900 lg:text-2xl">
-          A mais de <span className="font-bold text-yellow-600">22 anos</span>{' '}
+          Há mais de <span className="font-bold text-yellow-600">22 anos</span>{' '}
           criando ambientes seguros com{' '}
           <span className="font-bold text-yellow-600">excelência</span> no que
           fazemos!
         </h2>
 
         <span className="text-base font-normal text-slate-900 lg:text-xl">
-          Solicite uma cotação sem compromisso, e encontre as melhores condições
+          Solicite uma cotação sem compromisso e encontre as melhores condições
           para o seu veículo.
         </span>
 
--- src/components/SectionAccordeon.tsx
+++ src/components/SectionAccordeon.tsx
@@ -5,7 +5,6 @@ export function SectionAccordion() {
     <section className="bg-slate-100 flex flex-col py-10 px-5 items-center justify-center gap-10 lg:py-20 lg:px-80">
       <div className="py-5 px-[10px] bg-slate-50 flex items-center justify-center border-l-8 border-amber-500 box-border shadow-md">
         <span className="font-medium text-yellow-600 text-2xl">
-          Tire suas dúvidas! <br className="lg:hidden" />
           <strong className="text-cyan-900">
             Confira as principais coberturas para seu veículo!
           </strong>
