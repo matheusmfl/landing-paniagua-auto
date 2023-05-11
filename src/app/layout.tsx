e78f5cd0911afde824151b58707af29f2b03c1fc
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-05-11 18:54:51 -0300

    Create metaTags Keywords for SEO optimization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css'
 import { Roboto } from 'next/font/google'
 import { Header } from '@/components/Header'
 import { Analytics } from '@/components/Analytics'
+import Head from 'next/head'
 
 const roboto = Roboto({
   weight: ['300', '400', '500', '700'],
@@ -17,6 +18,31 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="pt-BR">
+      <Head>
+        <meta
+          name="keywords"
+          content="Seguro auto,
+          Seguro automóvel,
+          Cotação seguro auto,
+          Preço seguro auto,
+          Comprar seguro auto,
+          Melhor seguro auto,
+          Seguro auto online,
+          Seguro veicular,
+          Cobertura seguro auto,
+          Seguradora automóvel,
+          Seguro carro,
+          Seguro automotivo,
+          Proteção veicular,
+          Assistência 24 horas,
+          Franquia de seguro auto,
+          Seguro contra roubo e furto,
+          Seguro para terceiros,
+          Seguro de colisão,
+          Seguro de responsabilidade civil,
+          Seguro de danos próprios"
+        />
+      </Head>
       <body className={roboto.className}>
         <Analytics />
         <Header />
